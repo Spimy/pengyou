14ba7d741ccee85cd1747aaca57d097cf8a68770
Spimy <officialspyme@gmail.com>
2024-06-13 01:02:07 +0800

    📦 NEW: Add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section
+	class="flex flex-col items-center p-10 relative h-screen bg-gradient-to-b from-white to-primary"
+>
+	<header>
+		<h1 class="text-3xl md:text-5xl font-bold mb-2 text-center">PengYou</h1>
+		<p class="text-xl md:text-3xl font-bold mt-2 text-center">
+			Your best friend for all things budgeting!
+		</p>
+	</header>
+
+	<div class="space-y-4 md:space-y-10 mt-6 md:mt-10 w-full xl:w-[60dvw]">
+		<p
+			class="float-left font-bold text-sm md:text-base rounded-3xl bg-bracker p-4 text-white max-w-60"
+		>
+			<span class="text-orange-500">Not enough</span> money to get eco-friendly wood veneer?
+		</p>
+		<br />
+		<p
+			class="float-right font-bold text-sm md:text-base rounded-3xl bg-bracker p-4 text-white max-w-60"
+		>
+			Spent <span class="text-red-700">too much</span> on galvanized square steel?
+		</p>
+		<br />
+		<p
+			class="float-left font-bold text-sm md:text-base rounded-3xl bg-bracker p-4 text-white max-w-60"
+		>
+			Need to <span class="text-green-600">borrow</span> screws from your aunt?
+		</p>
+	</div>
+
+	<div>
+		<div
+			class="flex flex-col items-center gap-4 absolute bottom-[18%] md:bottom-1/4 left-2/4 w-80 md:w-fit -translate-x-2/4 bg-white p-5 rounded-3xl shadow-[-8px_8px_0px_-1px_rgba(5,76,214,25%)]"
+		>
+			<p class="font-bold text-lg md:text-2xl max-w-72 text-center">
+				Let <span class="text-primary">PengYou</span> teach you how to budget correctly!
+			</p>
+			<a
+				class="shadow-[-8px_8px_0px_-1px_rgba(0,0,0,1)] shadow-blue-700 hover:border-blue-500 transition ease-in-out delay-70 bg-primary hover:translate-y-1px active:translate-y-8 hover:scale-110 hover:bg-sky-500 duration-300 text-white font-bold text-lg md:text-2xl py-4 px-6 rounded-full focus:outline-none focus:shadow-outline"
+				href="/register"
+			>
+				Get Started
+			</a>
+		</div>
+
+		<img
+			class="absolute bottom-0 left-12 md:left-1/4 h-36 md:h-52"
+			src="/landing-page/pengyou-stick.png"
+			alt="Pengyou pointing"
+		/>
+	</div>
+</section>
+
+<section class="grid gap-4 bg-brack p-10">
+	<div
+		class="flex flex-col md:flex-row items-center justify-between bg-bracker rounded-3xl p-10 gap-4"
+	>
+		<div
+			class="grid place-items-center w-full h-full bg-white p-5 rounded-3xl shadow-[-8px_8px_0px_-1px_#ffffff50]"
+		>
+			<p class="font-bold text-lg md:text-2xl text-center">
+				Make PengYou proud of your financial achievements!
+			</p>
+		</div>
+		<img
+			class="w-full lg:w-1/4 rounded-3xl aspect-square grow"
+			src="/landing-page/beach.png"
+			alt="Pengyou at the beach"
+		/>
+	</div>
+	<div
+		class="flex flex-col md:flex-row items-center justify-between bg-bracker rounded-3xl p-10 gap-4"
+	>
+		<div
+			class="grid place-items-center w-full h-full bg-white p-5 rounded-3xl shadow-[-8px_8px_0px_-1px_#ffffff50]"
+		>
+			<p class="font-bold text-lg md:text-2xl text-center">Spoil PengYou with gifts!</p>
+		</div>
+		<img
+			class="w-full lg:w-1/4 rounded-3xl aspect-square grow"
+			src="/landing-page/snowy.png"
+			alt="Pengyou on snowy land"
+		/>
+	</div>
+</section>

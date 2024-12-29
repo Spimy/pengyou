030cfdc82d7c7790058c0fbe0f8fbaee9b609dc0
Spimy <williamlaw.3001@gmail.com>
2024-12-29 15:29:08 +0800

    👌 IMPROVE: Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +13,8 @@ const config = {
 
 if (process.env.ADAPTER === 'node') {
 	config.kit.adapter = adapterNode();
+} else if (process.env.ADAPTER === 'vercel') {
+	config.kit.adapter = adapterVercel();
 } else {
 	config.kit.adapter = adapter();
 }
